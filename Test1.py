0daf862c48e802c68c6549cac26c4885a5b3652e
Md Zahurul Alam <81699889+zahurul1989@users.noreply.github.com>
2021-04-10 13:43:48 +0600

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,17 @@
+row= int(input("Enter Row:"))
+column= int(input("Enter Column:"))
+
+r= 0
+
+while r < row:
+    c= 0
+    while c<column:
+        print("*",  end="")
+        c= c+1
+
+      print("")
+    r = r + 1
+
+
+
+
---
+++ Test2.py
@@ -0,0 +1,11 @@
+row= int(input("Enter row:"))
+
+r= 1
+
+while r <= row:
+    c= 0
+    while c < r:
+        print("*", end="")
+        c= c+1
+    print("")
+    r = r + 1
